069ee741ed94dc2eea73789e307bb2fbb818ce45
Amir <amir.ghandeharioon@gmail.com>
2018-03-05 14:13:02 +0000

    test for draw 1 couple only

--- test/appTest.js
+++ test/appTest.js
@@ -1,7 +1,6 @@
 const assert = require('chai').assert;
 const expect = require('chai').expect;
 
-// const game = new (require('../models/game'))();
 const Game = require('../models/game');
 
 describe('App', function () {
@@ -9,14 +8,12 @@ describe('App', function () {
     it('should return an array of a registered member', function () {
       let game = new Game()
       let result = game.addMember('Jack');
-      console.log("result:", result)
       expect(result).to.be.an('array').that.deep.include({ name: 'Jack', spouse: undefined });
 
     });
     it('should return an array of a registered couple', function () {
       let game = new Game()
       let result = game.addMember('Jack', 'Sarah');
-      console.log("result:", result)
       expect(result).to.be.an('array').that.deep.includes({ name: 'Jack', spouse: 'Sarah' }).and.deep.include({ name: 'Sarah', spouse: 'Jack' });
 
     });
@@ -28,6 +25,12 @@ describe('App', function () {
         let game = new Game()
         game.addMember('Jack');
         expect(game.draw.bind(game)).to.throw('*** Due to conditions, you can NOT use this app for only 1 person ***');
+      });
+
+      it('should throw an error if there is only 1 couple for draw', function () {
+        let game = new Game()
+        game.addMember('Jack', 'Sarah');
+        expect(game.draw.bind(game)).to.throw('*** Due to conditions, you can NOT use this app for only 1 couple ***');
       });
 
     });
